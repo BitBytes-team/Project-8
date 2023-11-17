4fb83fafcd9c61221db53b762466a631c40365f2
JAHNAVI KANUKUNTLA <116941985+Jahnavi-0305@users.noreply.github.com>
2023-11-17 01:34:55 -0500

    Update loginregister.jsx

--- components/loginregister/loginregister.jsx
+++ components/loginregister/loginregister.jsx
@@ -1,225 +1,207 @@
-import * as React from "react";
-import { Redirect } from 'react-router-dom';
-import axios from "axios";
-// eslint-disable-next-line import/no-unresolved
-import { Typography, Grid, FormControl, InputLabel, Input, Button } from "@material-ui/core";
+// LoginRegister.jsx
+import React, { Component } from 'react';
+import { Button, TextField, Typography, Paper } from '@mui/material';
 
-export default class LoginRegister extends React.Component {
+class LoginRegister extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      loginName: "",
-      password: "",
-      loginMessage: "", // use for prompting login info under login submit button
-      newLoginName: "",
-      firstName: "",
-      lastName: "",
-      description: "",
-      location: "",
-      occupation: "",
-      newPassword: "",
-      newPassword2: "",
-      registeredMessage: "", // use for prompting register info under register submit button
+      loginName: '',
+      // eslint-disable-next-line react/no-unused-state
+      password:'',
+      loginpassword: '',
+      confirmPassword: '', // new state for confirming password
+      firstName: '',
+      lastName: '',
+      location: '',
+      description: '',
+      occupation: '',
+      error: '',
     };
-    this.isLogged = null; // use for prompting user when login fails
   }
 
 
-  // Handle change of login name and password
-  handleInputChange = ({ target }) => this.setState({ [target.name]: target.value });
+  handleRegister = async () => {
+    // Implement registration logic here
+    try {
+      // Ensure passwords match
+      if (this.state.password !== this.state.confirmPassword) {
+        this.setState({ error: 'Passwords do not match' });
+        return;
+      }
 
-
-  /**
-  * Control login user form sumbit
-  * use Axios to send POST request with both login name and password to server
-  * * always use Axios to send request and update the users state variable
-  * */ 
-  handleLoginSubmit = e => { // async: to use "await" inside the func
-    e.preventDefault();  // prevent submit form and reload page behavior.
-    console.log("* Sending login Info to server: ", this.state.loginName + "  " + this.state.password + " *");
-
-    // construct a login user obj
-    const loginUser = {         
-      login_name: this.state.loginName, 
-      password: this.state.password 
-    };  
-
-    // axios to send a POST request with login info
-    axios
-      .post("/admin/login", loginUser) // POST request sent!
-      .then(response => {   // Handle success
-        console.log(`* LoginRegister: loggin Success! *`); 
-        this.setState({ loginMessage: response.data.message });
-        this.props.onLoginUserChange(response.data);  
-        // Pass back loggin user data and logged info to TopBar
-        // response.data format: { first_name: , _id: }
-      })
-      .catch(error => {     // Handle error
-        console.log(`* LoginRegister: loggin Fail! *`); 
-        console.log(error.response.data.message);
-        this.setState({ loginMessage: error.response.data.message });
-        this.props.onLoginUserChange(null);  
-        // Pass back info to TopBar
+      // Call your API to handle registration
+      const response = await fetch('/user', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          login_name: this.state.loginName,
+          loginpassword: this.state.loginpassword,
+          first_name: this.state.firstName,
+          last_name: this.state.lastName,
+          location: this.state.location,
+          description: this.state.description,
+          occupation: this.state.occupation,
+        }),
       });
-  };
 
-  /**
-   * To construct new user obj and clear old inputs.
-   * @returns a new user object for sending to server
-   */
-  getNewUser() {
-    // construct a new user obj
-    const newUser = {
-      login_name: this.state.newLoginName, 
-      password: this.state.newPassword,
-      first_name: this.state.firstName,
-      last_name: this.state.lastName,
-      location: this.state.location,
-      description: this.state.description,
-      occupation: this.state.occupation
-    };  
-    
-    // clear register inputs
-    this.setState({ 
-      newLoginName: "", 
-      newPassword: "",
-      newPassword2: "",
-      firstName: "",
-      lastName: "",
-      location: "",
-      description: "",
-      occupation: "",
-    });
-    return newUser;
-  }
-
-  /**
-  * Control new user form sumbit
-  * use Axios to send POST request to server to register a new user
-  * */ 
-  handleRegisterSubmit = e => {
-    e.preventDefault();
-
-    // make sure the two passwords entered are the same
-    if (this.state.newPassword !== this.state.newPassword2) {
-      this.setState({ registeredMessage: "The two passwords are NOT the same, please try again" }); // update message prompt to indicate user
-      return;
+      if (response.ok) {
+        // Registration successful
+        this.setState({
+          error: '',
+          loginName: '',
+          // eslint-disable-next-line react/no-unused-state
+          loginpassword: '',
+          confirmPassword: '',
+          firstName: '',
+          lastName: '',
+          location: '',
+          description: '',
+          occupation: '',
+        });
+        // Optionally, you can provide a success message
+      } else {
+        // Registration failed
+        const errorMessage = await response.text();
+        this.setState({ error: errorMessage });
+      }
+    } catch (error) {
+      console.error('Registration error:', error);
+      this.setState({ error: 'An error occurred during registration' });
     }
-    const newUser = this.getNewUser();
-    console.log(newUser);
-    
-    // axios to send a POST request with login info
-    axios
-      .post("/user", newUser) // POST request sent!
-      .then(response => {   // Handle success
-        console.log(`* LoginRegister: new User register Success! *`); 
-        this.setState({ registeredMessage: response.data.message }); // update message prompt to indicate user
-      })
-      .catch(error => {     // Handle error
-        console.log(`* LoginRegister: new User loggin Fail! *`); 
-        this.setState({ registeredMessage: error.response.data.message }); // update error prompt to indicate user
-      });
   };
-
-
-  customForm(inputLabel, id, type, value, required, autoFocus = false) {
-     return (
-       <FormControl fullWidth>
-         <InputLabel htmlFor={id}>{inputLabel}</InputLabel>
-         <Input
-           name={id}
-           id={id}
-           autoFocus={autoFocus}
-           autoComplete="on"
-           type={type}
-           value={value}
-           onChange={this.handleInputChange}
-           required={required}
-         />
-       </FormControl>
-     );
-  }
   
+  
+  handleLogin = async () => {
+    // Implement login logic here
+    try {
+      // Call your API to handle login
+      const response = await fetch('/admin/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          login_name: this.state.loginName,
+          password: this.state.password,
+        }),
+      });
 
-  render() {
-    // Handle jumping into the login User's detail page if user is authorizedx
-    const loginUser = this.props.loginUser;
-    if (loginUser) {
-      return <Redirect from="/login-register" to={`/users/${loginUser._id}`} />;
-       // the state prop in <Redirect> is used to pass data between components when using client-side routing
+      if (response.ok) {
+        const user = await response.json();
+        this.props.changeUser(user);
+      } else {
+        this.setState({ error: 'Invalid login credentials' });
+      }
+    } catch (error) {
+      console.error('Login error:', error);
+      this.setState({ error: 'An error occurred during login' });
     }
-
-    // else, show login page:
+  };
+  
+  render() {
     return (
-      <Grid container >
-
-        {/* Login Form */}
-        <Grid container item direction="column" alignItems="center" xs={6}>
-          {/* Welcome prompt */}
-          <Typography variant="h5">Log In</Typography>
-          {/* login inputs */}
-          <Grid item xs={8}>
-            <form onSubmit={this.handleLoginSubmit}>
-              {this.customForm("Login Name", "loginName", "text", this.state.loginName, true, true)}
-              {this.customForm("Password", "password", "password", this.state.password, true)}
-              <br/><br/>
-              <Button
-                type="submit"
-                disabled={this.state.loginName.length === 0}
-                fullWidth
-                variant="contained"
-                color="primary"
-              >
-                Login
-              </Button>
-              <br/><br/>
-              {this.state.loginMessage && (
-                <Typography style={{ color: "red" }}>
-                  {this.state.loginMessage}
-                </Typography>
-              )}
-            </form>
-          </Grid>
-        </Grid>
-
-        {/* Register Form */}
-        <Grid container item direction="column" alignItems="center" xs={6}>
-          {/* Welcome prompt */}
-          <Typography variant="h5">Create New Account</Typography>
-          {/* Register inputs */}
-          <Grid item xs={8}>
-            <form onSubmit={this.handleRegisterSubmit}>
-              {this.customForm("New Login Name*", "newLoginName", "text", this.state.newLoginName, true)}
-              {this.customForm("First Name*", "firstName", "text", this.state.firstName, true)}
-              {this.customForm("Last Name*", "lastName", "text", this.state.lastName, true)}
-              {this.customForm("Description", "description", "text", this.state.description)}
-              {this.customForm("Location", "location", "text", this.state.location)}
-              {this.customForm("Occupation", "occupation", "text", this.state.occupation)}
-              {this.customForm("New Password*", "newPassword", "password", this.state.newPassword, true)}
-              {this.customForm("Re-enter Password*", "newPassword2", "password", this.state.newPassword2, true)}
-              <br/><br/>
-              <Button
-                type="submit"
-                disabled={this.state.newLoginName.length === 0}
-                fullWidth
-                variant="contained"
-                color="primary"
-              >
-                Register Me
-              </Button>
-              <br/><br/>
-              {this.state.registeredMessage && (
-                <Typography style={{ color: this.state.registeredMessage.includes("successfully") ? "green" : "red" }}>
-                  {this.state.registeredMessage}
-                </Typography>
-              )}
-
-              {/* The two passwords are not the same prompt */}
-            </form>
-          </Grid>
-        </Grid>
-
-      </Grid>
+      <Paper className="main-grid-item">
+        <Typography variant="h5">Please Login</Typography>
+        <TextField
+          id='login_name'
+          label="Login Name"
+          variant="outlined"
+          fullWidth
+          value={this.state.loginName}
+          onChange={(e) => this.setState({ loginName: e.target.value })}
+        />
+        <TextField
+          id='password'
+          label="Password"
+          variant="outlined"
+          fullWidth
+          type="password"
+          value={this.state.password}
+          onChange={(e) => this.setState({ password: e.target.value })}
+        />
+        <Button variant="contained" onClick={this.handleLogin}>
+          Login
+        </Button>
+        <Typography variant="h5">Not registered? </Typography>
+        <Typography variant="h5">Register here: </Typography>
+        <TextField
+          id='newPassword'
+          label="Enter new Password"
+          variant="outlined"
+          fullWidth
+          type="password"
+          value={this.state.loginpassword}
+          // eslint-disable-next-line react/no-unused-state
+          onChange={(e) => this.setState({ loginpassword: e.target.value })}
+        />
+        <TextField
+          id='confirmPassword'
+          label="Confirm Password"
+          variant="outlined"
+          fullWidth
+          type="password"
+          value={this.state.confirmPassword}
+          onChange={(e) => this.setState({ confirmPassword: e.target.value })}
+        />
+        <TextField
+          id='firstName'
+          label="First Name"
+          variant="outlined"
+          fullWidth
+          value={this.state.firstName}
+          onChange={(e) => this.setState({ firstName: e.target.value })}
+        />
+        <TextField
+          id='lastName'
+          label="Last Name"
+          variant="outlined"
+          fullWidth
+          value={this.state.lastName}
+          onChange={(e) => this.setState({ lastName: e.target.value })}
+        />
+        <TextField
+          id='location'
+          label="Location"
+          variant="outlined"
+          fullWidth
+          value={this.state.location}
+          onChange={(e) => this.setState({ location: e.target.value })}
+        />
+        <TextField
+          id='description'
+          label="Description"
+          variant="outlined"
+          fullWidth
+          value={this.state.description}
+          onChange={(e) => this.setState({ description: e.target.value })}
+        />
+        <TextField
+          id='occupation'
+          label="Occupation"
+          variant="outlined"
+          fullWidth
+          value={this.state.occupation}
+          onChange={(e) => this.setState({ occupation: e.target.value })}
+        />
+        <Button variant="contained" onClick={this.handleRegister}>
+          Register Me
+        </Button>
+        {this.state.error && (
+          <Typography variant="body2" color="error" style={{ marginTop: '8px' }}>
+            {this.state.error}
+          </Typography>
+        )}
+        {this.state.error && (
+          <Typography variant="body2" color="error" style={{ marginTop: '8px' }}>
+            {this.state.error}
+          </Typography>
+        )}
+      </Paper>
     );
   }
 }
+
+export default LoginRegister;
