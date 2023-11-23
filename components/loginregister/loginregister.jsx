1fa41e7666049ef97e40780456591b4da44869f8
Sharu201101 <143195829+Sharu201101@users.noreply.github.com>
2023-11-22 19:07:56 -0500

    Update loginregister.jsx

--- components/loginregister/loginregister.jsx
+++ components/loginregister/loginregister.jsx
@@ -1,207 +1,260 @@
-// LoginRegister.jsx
-import React, { Component } from 'react';
-import { Button, TextField, Typography, Paper } from '@mui/material';
+import React, { useState } from 'react';
+import {
+  Button,
+  Box,
+  TextField,
+  Alert,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions
+} from '@mui/material';
+import axios from 'axios';
 
-class LoginRegister extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      loginName: '',
-      // eslint-disable-next-line react/no-unused-state
-      password:'',
-      loginpassword: '',
-      confirmPassword: '', // new state for confirming password
-      firstName: '',
-      lastName: '',
-      location: '',
-      description: '',
-      occupation: '',
-      error: '',
-    };
-  }
+const LoginRegister = ({ changeUser }) => {
+  const [user, setUser] = useState({
+    first_name: '',
+    last_name: '',
+    location: '',
+    description: '',
+    occupation: '',
+    login_name: '',
+    password: '',
+    password_repeat: ''
+  });
+  const [showLoginError, setShowLoginError] = useState(false);
+  const [showRegistrationError, setShowRegistrationError] = useState(false);
+  const [showRegistrationSuccess, setShowRegistrationSuccess] = useState(false);
+  const [openRegistration, setOpenRegistration] = useState(false);
+  const [showRequiredFieldsWarning, setShowRequiredFieldsWarning] = useState(false);
 
-
-  handleRegister = async () => {
-    // Implement registration logic here
-    try {
-      // Ensure passwords match
-      if (this.state.password !== this.state.confirmPassword) {
-        this.setState({ error: 'Passwords do not match' });
-        return;
-      }
-
-      // Call your API to handle registration
-      const response = await fetch('/user', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          login_name: this.state.loginName,
-          loginpassword: this.state.loginpassword,
-          first_name: this.state.firstName,
-          last_name: this.state.lastName,
-          location: this.state.location,
-          description: this.state.description,
-          occupation: this.state.occupation,
-        }),
-      });
-
-      if (response.ok) {
-        // Registration successful
-        this.setState({
-          error: '',
-          loginName: '',
-          // eslint-disable-next-line react/no-unused-state
-          loginpassword: '',
-          confirmPassword: '',
-          firstName: '',
-          lastName: '',
-          location: '',
-          description: '',
-          occupation: '',
-        });
-        // Optionally, you can provide a success message
-      } else {
-        // Registration failed
-        const errorMessage = await response.text();
-        this.setState({ error: errorMessage });
-      }
-    } catch (error) {
-      console.error('Registration error:', error);
-      this.setState({ error: 'An error occurred during registration' });
-    }
+  const handleShowRegistration = () => {
+    setOpenRegistration(true);
+    setShowRegistrationError(false);
+    setShowRequiredFieldsWarning(false);
   };
-  
-  
-  handleLogin = async () => {
-    // Implement login logic here
-    try {
-      // Call your API to handle login
-      const response = await fetch('/admin/login', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          login_name: this.state.loginName,
-          password: this.state.password,
-        }),
-      });
 
-      if (response.ok) {
-        const user = await response.json();
-        this.props.changeUser(user);
-      } else {
-        this.setState({ error: 'Invalid login credentials' });
-      }
-    } catch (error) {
-      console.error('Login error:', error);
-      this.setState({ error: 'An error occurred during login' });
-    }
+  const handleCloseRegistration = () => {
+    setOpenRegistration(false);
+    setShowRegistrationSuccess(false);
+    setShowRegistrationError(false);
+    setShowRequiredFieldsWarning(false);
   };
-  
-  render() {
-    return (
-      <Paper className="main-grid-item">
-        <Typography variant="h5">Please Login</Typography>
-        <TextField
-          id='login_name'
-          label="Login Name"
-          variant="outlined"
-          fullWidth
-          value={this.state.loginName}
-          onChange={(e) => this.setState({ loginName: e.target.value })}
-        />
-        <TextField
-          id='password'
-          label="Password"
-          variant="outlined"
-          fullWidth
-          type="password"
-          value={this.state.password}
-          onChange={(e) => this.setState({ password: e.target.value })}
-        />
-        <Button variant="contained" onClick={this.handleLogin}>
-          Login
-        </Button>
-        <Typography variant="h5">Not registered? </Typography>
-        <Typography variant="h5">Register here: </Typography>
-        <TextField
-          id='newPassword'
-          label="Enter new Password"
-          variant="outlined"
-          fullWidth
-          type="password"
-          value={this.state.loginpassword}
-          // eslint-disable-next-line react/no-unused-state
-          onChange={(e) => this.setState({ loginpassword: e.target.value })}
-        />
-        <TextField
-          id='confirmPassword'
-          label="Confirm Password"
-          variant="outlined"
-          fullWidth
-          type="password"
-          value={this.state.confirmPassword}
-          onChange={(e) => this.setState({ confirmPassword: e.target.value })}
-        />
-        <TextField
-          id='firstName'
-          label="First Name"
-          variant="outlined"
-          fullWidth
-          value={this.state.firstName}
-          onChange={(e) => this.setState({ firstName: e.target.value })}
-        />
-        <TextField
-          id='lastName'
-          label="Last Name"
-          variant="outlined"
-          fullWidth
-          value={this.state.lastName}
-          onChange={(e) => this.setState({ lastName: e.target.value })}
-        />
-        <TextField
-          id='location'
-          label="Location"
-          variant="outlined"
-          fullWidth
-          value={this.state.location}
-          onChange={(e) => this.setState({ location: e.target.value })}
-        />
-        <TextField
-          id='description'
-          label="Description"
-          variant="outlined"
-          fullWidth
-          value={this.state.description}
-          onChange={(e) => this.setState({ description: e.target.value })}
-        />
-        <TextField
-          id='occupation'
-          label="Occupation"
-          variant="outlined"
-          fullWidth
-          value={this.state.occupation}
-          onChange={(e) => this.setState({ occupation: e.target.value })}
-        />
-        <Button variant="contained" onClick={this.handleRegister}>
-          Register Me
-        </Button>
-        {this.state.error && (
-          <Typography variant="body2" color="error" style={{ marginTop: '8px' }}>
-            {this.state.error}
-          </Typography>
+
+  const handleLogin = () => {
+    const currentState = JSON.stringify(user);
+    axios
+      .post('/admin/login', currentState, {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+      .then((response) => {
+        const userData = response.data;
+        setShowLoginError(false);
+        setShowRegistrationSuccess(false);
+        setShowRegistrationError(false);
+        changeUser(userData);
+      })
+      .catch((error) => {
+        setShowLoginError(true);
+        setShowRegistrationSuccess(false);
+        setShowRegistrationError(false);
+        console.log(error);
+      });
+  };
+
+  const handleRegister = () => {
+    if (user.password !== user.password_repeat) {
+      setShowRegistrationError(true);
+      setShowRequiredFieldsWarning(false);
+      alert("Passwords don't match");
+      return;
+    }
+
+    const requiredFields = [
+      'login_name',
+      'password',
+      'password_repeat',
+      'first_name',
+      'last_name'
+    ];
+    if (requiredFields.some((field) => !user[field])) {
+      setShowRequiredFieldsWarning(true);
+      setShowRegistrationError(false);
+      return;
+    }
+
+    const currentState = JSON.stringify(user);
+    axios
+      .post('/user/', currentState, {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+      .then((response) => {
+        const userData = response.data;
+        setShowRegistrationSuccess(true);
+        setShowRegistrationError(false);
+        setShowLoginError(false);
+        setOpenRegistration(false);
+        setShowRequiredFieldsWarning(false);
+        changeUser(userData);
+      })
+      .catch((error) => {
+        setShowRegistrationError(true);
+        setShowLoginError(false);
+        setShowRegistrationSuccess(false);
+        setShowRequiredFieldsWarning(false);
+        console.log(error);
+      });
+  };
+
+  const handleChange = (event) => {
+    setUser({
+      ...user,
+      [event.target.id]: event.target.value
+    });
+  };
+
+  return (
+    <div>
+      <Box component="form" autoComplete="off">
+        {showLoginError && <Alert severity="error">Login Failed</Alert>}
+        {showRegistrationError && (
+          <Alert severity="error">Please correct the registration details.</Alert>
         )}
-        {this.state.error && (
-          <Typography variant="body2" color="error" style={{ marginTop: '8px' }}>
-            {this.state.error}
-          </Typography>
+        {showRegistrationSuccess && (
+          <Alert severity="success">Registration Succeeded</Alert>
         )}
-      </Paper>
-    );
-  }
-}
+        <div>
+          <TextField
+            id="login_name"
+            label="Login Name"
+            variant="outlined"
+            fullWidth
+            margin="normal"
+            required={true}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <TextField
+            id="password"
+            label="Password"
+            variant="outlined"
+            fullWidth
+            margin="normal"
+            type="password"
+            required={true}
+            onChange={handleChange}
+          />
+        </div>
+        <Box mb={2}>
+          <Button type="submit" variant="contained" onClick={handleLogin}>
+            Login
+          </Button>
+        </Box>
+      </Box>
+      <Box>
+        <Button variant="contained" onClick={handleShowRegistration}>
+          Register
+        </Button>
+        <Dialog open={openRegistration} onClose={handleCloseRegistration}>
+          <DialogTitle>User Registration</DialogTitle>
+          <DialogContent>
+            {showRequiredFieldsWarning && (
+              <Alert severity="warning">Please fill in all required fields.</Alert>
+            )}
+            <Box>
+              <TextField
+                id="login_name"
+                label="Login Name"
+                variant="outlined"
+                fullWidth
+                margin="normal"
+                required={true}
+                onChange={handleChange}
+              />
+              <TextField
+                id="password"
+                label="Password"
+                variant="outlined"
+                fullWidth
+                margin="normal"
+                type="password"
+                required={true}
+                onChange={handleChange}
+              />
+              <TextField
+                id="password_repeat"
+                label="Repeat Password"
+                variant="outlined"
+                fullWidth
+                margin="normal"
+                type="password"
+                required={true}
+                onChange={handleChange}
+              />
+              <TextField
+                id="first_name"
+                label="First Name"
+                variant="outlined"
+                fullWidth
+                margin="normal"
+                autoComplete="off"
+                required={true}
+                onChange={handleChange}
+              />
+              <TextField
+                id="last_name"
+                label="Last Name"
+                variant="outlined"
+                fullWidth
+                margin="normal"
+                required={true}
+                onChange={handleChange}
+              />
+              <TextField
+                id="location"
+                label="Location"
+                variant="outlined"
+                fullWidth
+                margin="normal"
+                onChange={handleChange}
+              />
+              <TextField
+                id="description"
+                label="Description"
+                variant="outlined"
+                multiline
+                rows={4}
+                fullWidth
+                margin="normal"
+                onChange={handleChange}
+              />
+              <TextField
+                id="occupation"
+                label="Occupation"
+                variant="outlined"
+                fullWidth
+                margin="normal"
+                onChange={handleChange}
+              />
+            </Box>
+          </DialogContent>
+          <DialogActions>
+            <Button variant="contained" onClick={handleRegister}>
+              Register Me
+            </Button>
+            <Button variant="outlined" onClick={handleCloseRegistration}>
+              Close
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </Box>
+    </div>
+  );
+};
 
 export default LoginRegister;
